8e2f76913c02bf579a460145ac73380a421d85aa
Abani Das <abanidas99@gmail.com>
2019-09-29 19:37:59 +0530

    minor update

--- scripts/work-util.js
+++ scripts/work-util.js
@@ -3,9 +3,12 @@ $(".proj").click(function() {
 		location.href = "https://abanidas.github.io/proj_lpo";
 	}
 	else if (this.id == "proj_2") {
-		window.open("https://github.com/abanidas/ByjuCloneApp", "_blank");
+		window.open("https://play.google.com/store/apps/details?id=com.abani.bbc.bbcorder&hl=en", "_blank");
 	}
 	else if (this.id == "proj_3") {
 		window.open("https://github.com/abanidas/PopularMoviesApp", "_blank");
+	}
+	else if (this.id == "proj_4") {
+		window.open("https://github.com/abanidas/ByjuCloneApp", "_blank");
 	}
 });
